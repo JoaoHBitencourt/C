6d41e74fabb9b1787a89cf0fb91cd34c8da1b6c5
Joao Henrique Bitencourt <jh.bitencourt23@gmail.com>
2023-06-03 22:07:27 -0300

    Algoritimos em Java

---
+++ Java/casting/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Locale;
+public class Main {
+
+	public static void main(String[] args) {
+
+		        Locale.setDefault(Locale.US);
+
+		        double a;
+		        int b;
+
+		        a = 5.0;
+		        b = (int) a;
+
+		        System.out.println(b);
+		       
+		        //
+		        
+		        int x, y; 5 
+		        x = 5; 10 
+		        y = 2 * x; 
+		        System.out.println(x); 
+		        System.out.println(y);
+		        
+		        //
+		        
+		        int x; 5 
+		        double y; 10.0 
+		        x = 5; 
+		        y = 2 * x; 
+		        System.out.println(x); 
+		        System.out.println(String.format("%.1f", y));
+		        
+		        //
+		        
+		        double b1, b2, h, area; 35.0 
+		        b1 = 6.0; 
+		        b2 = 8.0; 
+		        h = 5.0; 
+		        area = (b1 + b2) / 2.0 * h; 
+		        System.out.println(area); 
+		        
+		        //
+		        
+		        int a, b, resultado; 2 
+		        a = 5; 
+		        b = 2; 
+		        resultado = a / b; 
+		        System.out.println(resultado);
+		       
+		 
+		    }
+}
