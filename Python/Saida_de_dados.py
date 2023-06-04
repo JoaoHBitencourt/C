7b294ee2b1ec766eb1b904a45d6deae8513e3406
Joao Henrique Bitencourt <jh.bitencourt23@gmail.com>
2023-06-03 22:10:21 -0300

    Algoritimos em Python

---
+++ Python/Saida_de_dados.py
@@ -0,0 +1,20 @@
+print("Bom Dia!")
+print("Boa Noite!")
+
+print("Bom Dia!", end="")
+print("Boa Noite!")
+
+x = 10
+y = 20
+print(x)
+print(y)
+
+a = 2.3456
+print("{:.2f}".format(a))
+
+idade = 32
+salario = 4560.9
+nome = "Maria Silva"
+sexo = 'F'
+
+print(f"A funcionaria {nome}, sexo {sexo}, ganha {salario:.2f} e tem {idade} anos")
---
+++ Python/casting.py
@@ -0,0 +1,28 @@
+x: int
+y: float
+
+x = 5
+y = 2 * x
+
+print(x)
+print(f"{y:.1f}")
+
+print()
+b1 = 6.0
+b2 = 8.0
+h = 5.0
+area = (b1 + b2) / 2.0 * h
+print(area)
+
+print()
+a = 5
+b = 2
+resultado = a // b
+print(resultado)
+
+print()
+c = 5.2
+d = int(c)
+print(d)
+
+print()
